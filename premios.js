1897a450a898d0adbe794ee38f7a6aaeb0691492
jonaalbesanchez25-gif <jonaalbesanchez25@gmail.com>
2026-07-02 20:54:18 -0300

    Update premios.js

--- premios.js
+++ premios.js
@@ -1,10 +1,11 @@
 // EDITÁ AQUÍ los premios y cantidades.
 // Después de cambiar, guardá el archivo y volvé a subirlo a GitHub.
 const PREMIOS = [
-  {nombre:"¡Ganaste una Vianda Compartir!", emoji:"🏆", cantidad:1, gana:true},
-  {nombre:"¡Ganaste una Hamburguesa completa!", emoji:"🍔", cantidad:2, gana:true},
+  {nombre:"¡Ganaste una Vianda Compartir!", emoji:"🏆", cantidad:10, gana:true},
+  {nombre:"¡Ganaste una Vianda Simple!", emoji:"🫘", cantidad:10, gana:true},
   {nombre:"¡Ganaste Papas fritas!", emoji:"🍟", cantidad:5, gana:true},
-  {nombre:"¡Ganaste una Gaseosa!", emoji:"🥤", cantidad:10, gana:true},
-  {nombre:"¡Ganaste 15% de descuento!", emoji:"🎟️", cantidad:20, gana:true},
-  {nombre:"Esta vez no hubo premio. ¡Seguí participando!", emoji:"❤️", cantidad:62, gana:false}
-];
+  {nombre:"¡Ganaste Una Pizza!", emoji:"🍕", cantidad:5, gana:true},
+  {nombre:"¡Ganaste 30% de descuento!", emoji:"🎟️", cantidad:10, gana:true},
+  {nombre:"¡Ganaste un Sandwich!", emoji:"🍔", cantidad:5, gana: true},
+  {nombre:"Esta vez no hubo premio. ¡Seguí participando!", emoji:"❤️", cantidad:1000, gana:false}
+] 
